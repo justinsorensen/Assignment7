4b4b3e83cc9ca4029f0ae6d46885680a0729e431
Justin Sorensen <justinsorensen83@gmail.com>
2013-02-12 03:11:57 -0800

    Home computer work commit

---
+++ server_test.js
@@ -0,0 +1,52 @@
+var http    = require('http');
+var fs      = require('fs');
+
+var server = http.createServer(function (request, response) {
+
+	//Serving the static files
+    if (request.method === "GET"){
+				//Files will be stored in "public" folder
+        var url = './public' + (request.url == '/' ? '/index.html' : request.url);
+				
+				//Using included "file system" functionality
+				//Other plugins also available to handle these kinds of requests
+				
+		//fs.readFile(filename,[encoding],[callback])		
+        fs.readFile(url, function(error, content) {
+            if(error) {
+                console.log('error loading file ' + url + ': ', error);
+                response.writeHead(404);
+                response.end();
+            }else {
+                // Check mime type
+                var temp     = url.lastIndexOf(".");
+                var ext     = url.substring((temp));
+                var mime    = mimes[ext] || 'text/plain';
+				
+                // Write the file
+				//response.writeHead(statusCode,[reasonPhrase],[headers])
+                response.writeHead(200, { 'Content-Type': mime });
+				
+				//response.end([data],[encoding])
+                response.end(content, 'utf-8');
+            }
+        });
+    }
+});
+ 
+// Listen to port 5000, log to console
+server.listen(5000, function() {
+  console.log('Listening to port ' + server.address().port);
+  });
+ 
+ //Mime type definitions
+var mimes = {
+    '.css' :  'text/css',
+    '.js' :   'text/javascript',
+    '.htm' :  'text/html',
+    '.html' : 'text/html',
+	'.png' : 'image/png',
+	'.gif' : 'image/gif',
+	'.jpg' : 'image/jpg',
+	'.bmp' : 'image/bmp'
+};
